48f659db2341bd80bcc46bb02370d3d5b314f942
hyunganom <dlguswo0504@naver.com>
2022-08-23 23:06:24 +0900

    DAO 작성중

--- src/main/java/com/fastcampus/ch4/dao/BoardDao.java
+++ src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -1,16 +1,8 @@
 package com.fastcampus.ch4.dao;
 
 import com.fastcampus.ch4.domain.BoardDto;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class BoardDao {
-    @Autowired
-    SqlSession session;
-
-    String namespace="com.fastcampus.ch4.dao.BoardMapper.";
-
-    BoardDto select(int bno) throws Exception {
-        return session.selectOne( namespace+"select",bno);
-    }
+public interface BoardDao {
+    BoardDto select(int bno) throws Exception;
+    BoardDto delete(Integer bno, String writer) throws  Exception;
 }
---
+++ src/main/java/com/fastcampus/ch4/dao/BoardDaoImpl.java
@@ -0,0 +1,17 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BoardDaoImpl implements BoardDao {
+    @Autowired
+    SqlSession session;
+
+    String namespace="com.fastcampus.ch4.dao.BoardMapper.";
+
+    @Override
+    public BoardDto select(int bno) throws Exception {
+        return session.selectOne( namespace+"select",bno);
+    }
+}
